573a631c971e6af8feab1565aaecad2a448087dd
luciaaroca <lucia.aroca2510@gmail.com>
2025-12-05 18:17:06 +0100

    menu

--- frontend/src/components/Header/Nav/Nav.jsx
+++ frontend/src/components/Header/Nav/Nav.jsx
@@ -1,26 +1,33 @@
 import React, {useContext} from "react";
+import {  slide as Menu } from "react-burger-menu";
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../../../App'; // importamos desde App.js
+import "./Nav.css";
 
 const Nav = () => {
 
   //Contexto
   const { isLogged } = useContext(AuthContext);
 
-  return <nav>
-     <li><Link to="/">Home</Link></li>
-     <li><Link to="/ligas">Ligas</Link></li>
-    <li><Link to="/retro">Retro</Link></li>
-    <li><Link to="/especiales">Especiales</Link></li>
-    <li><Link to="/favorites">Favoritos</Link></li>
-    <li><Link to="/profile">Perfil</Link></li>
-    <li><Link to="/carrito">Carrito</Link></li>
-    <li><Link to="/contact">Contacto</Link></li>
-    {isLogged && <li>Usuario logueado</li>}
-   
-    
+  return (
+    <div >
+    <nav className="barNav">
+      <p>GolWear</p>
+    </nav>
+    <Menu right>
+        <Link to="/" className="menu-item">Home</Link>
+        <Link to="/ligas" className="menu-item">Ligas</Link>
+        <Link to="/retro" className="menu-item">Retro</Link>
+        <Link to="/especiales" className="menu-item">Especiales</Link>
+        <Link to="/favorites" className="menu-item">Favoritos</Link>
+        <Link to="/profile" className="menu-item">Perfil</Link>
+        <Link to="/carrito" className="menu-item">Carrito</Link>
+        <Link to="/contact" className="menu-item">Contacto</Link>
+        {isLogged && <span className="menu-item">Usuario logueado</span>}
+    </Menu>
+    </div>
+    ) 
      
-    </nav>;
 };
 
 export default Nav;
