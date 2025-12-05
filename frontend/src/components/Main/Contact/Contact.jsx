3dd38054df9154967f97028d3c3efa828bd27c84
luciaaroca <lucia.aroca2510@gmail.com>
2025-12-05 14:07:56 +0100

    contact/carrito

--- frontend/src/components/Main/Contact/Contact.jsx
+++ frontend/src/components/Main/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = () => {
   return <section>
-    <h1>Bienvenido a [Nombre de tu tienda].</h1>
+    <h1>Bienvenido a GolWear</h1>
     <p>Aquí puedes comprar camisetas de fútbol de tus equipos favoritos. Si tienes dudas, algún problema con tu pedido o quieres una camiseta especial, escríbenos (dmarrquez6@gmail.com)y te ayudaremos.</p>
     </section>;
 };
